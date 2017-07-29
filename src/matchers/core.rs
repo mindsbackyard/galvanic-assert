440410989647c9da8b70770173ef4ca0c94858f5
Christopher Bacher <mindsbackyard@gmail.com>
2017-07-29 17:27:21 +0200

    add examples to core matcher docs

--- src/matchers/core.rs
+++ src/matchers/core.rs
@@ -32,36 +32,101 @@ macro_rules! matchresult_from_comparison {
 }
 
 /// A matcher which always matches.
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), assertion_always_succeeds());
+/// # }
 pub fn assertion_always_succeeds<'a,T:'a>() -> Box<Matcher<'a,T> + 'a> {
     Box::new(|_s: &T| MatchResultBuilder::for_("succeeds_always").matched())
 }
-pub fn any_value<'a,T:'a>() -> Box<Matcher<'a,T> + 'a> { assertion_always_succeeds() }
+/// A matcher which always matches.
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), any_value());
+/// # }
+pub fn any_value<'a,T:'a>() -> Box<Matcher<'a,T> + 'a> {
+    Box::new(|_s: &T| MatchResultBuilder::for_("any_value").matched())
+}
 
 /// A matcher which never matches.
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(
+///     assert_that!(&(1+1), assertion_always_fails()),
+///     panics
+/// );
+/// # }
 pub fn assertion_always_fails<'a,T:'a>() -> Box<Matcher<'a,T> + 'a> {
     Box::new(|_s: &T| {
         MatchResultBuilder::for_("fails_always").failed_because("This matcher fails always")
     })
 }
+/// A matcher which never matches.
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(
+///     assert_that!(&(1+1), no_value()),
+///     panics
+/// );
+/// # }
 pub fn no_value<'a,T:'a>() -> Box<Matcher<'a,T> + 'a> { assertion_always_fails() }
 
 /// Accepts a matcher and returns it unmodified.
 ///
 /// This is just syntactic sugar.
-pub fn is<'a, T:'a, M>(matcher: M) -> M
-where M: Matcher<'a,T> {
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), is(eq(2)));
+/// # }
+pub fn is<'a, T:'a>(matcher: Box<Matcher<'a,T> + 'a>) -> Box<Matcher<'a,T> + 'a> {
     matcher
 }
 
 /// Accepts a matcher and returns it unmodified.
 ///
 /// This is just syntactic sugar.
-pub fn has<'a, T:'a, M>(matcher: M) -> M
-where M: Matcher<'a,T> {
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// let vs = vec![1, 2];
+/// assert_that!(&vs.len(), has(lt(3)));
+/// # }
+pub fn has<'a, T:'a>(matcher: Box<Matcher<'a,T> + 'a>) -> Box<Matcher<'a,T> + 'a> {
     matcher
 }
 
 /// A matcher negating the result of the passed matcher.
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), not(eq(3)));
+/// # }
 pub fn not<'a, T: 'a>(matcher: Box<Matcher<'a,T> + 'a>) -> Box<Matcher<'a,T> + 'a> {
     Box::new(move |actual: &'a T| {
         match matcher.check(actual) {
@@ -77,49 +142,160 @@ pub fn not<'a, T: 'a>(matcher: Box<Matcher<'a,T> + 'a>) -> Box<Matcher<'a,T> + '
 /// Matches if the asserted value is equal to the expected value.
 ///
 /// This matcher should not be used when asserting floating point values.
-/// Use [close_to] instead.
+/// Use [close_to] instead. This is the same as [eq].
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), equal_to(2));
+/// # }
 pub fn equal_to<'a, T>(expected: T) -> Box<Matcher<'a,T> + 'a>
 where T: PartialEq + Debug + 'a {
     Box::new(move |actual: &T| matchresult_from_comparison!(actual == expected, "equal"))
 }
 /// Matches if the asserted value is equal to the expected value.
+///
+/// This matcher should not be used when asserting floating point values.
+/// Use [close_to] instead. This is the same as [equal_to].
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), eq(2));
+/// # }
 pub fn eq<'a, T: PartialEq + Debug + 'a>(expected: T) -> Box<Matcher<'a,T> + 'a> { equal_to(expected) }
 
 /// Matches if the asserted value is less than the expected value.
+///
+/// This is the same as [lt].
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), less_than(3));
+/// # }
 pub fn less_than<'a, T>(expected: T) -> Box<Matcher<'a,T> + 'a>
 where T: PartialOrd + Debug + 'a {
     Box::new(move |actual: &T| matchresult_from_comparison!(actual < expected, "less_than"))
 }
 /// Matches if the asserted value is less than the expected value.
+///
+/// This is the same as [less_than].
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), less_than(3));
+/// # }
 pub fn lt<'a, T: PartialOrd + Debug + 'a>(expected: T) -> Box<Matcher<'a,T> + 'a> { less_than(expected) }
 
 /// Matches if the asserted value is greater than the expected value.
+///
+/// This is the same as [gt].
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), greater_than(1));
+/// # }
 pub fn greater_than<'a, T>(expected: T) -> Box<Matcher<'a,T> + 'a>
 where T: PartialOrd + Debug + 'a {
     Box::new(move |actual: &T| matchresult_from_comparison!(actual > expected, "greater_than"))
 }
 /// Matches if the asserted value is greater than the expected value.
+///
+/// This is the same as [greater_than].
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), gt(1));
+/// # }
 pub fn gt<'a, T: PartialOrd + Debug + 'a>(expected: T) -> Box<Matcher<'a,T> + 'a> { greater_than(expected) }
 
 /// Matches if the asserted value is less than or equal to the expected value.
+///
+/// This is the same as [leq].
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), less_than_or_equal(3));
+/// assert_that!(&(1+1), less_than_or_equal(2));
+/// # }
 pub fn less_than_or_equal<'a, T>(expected: T) -> Box<Matcher<'a,T> + 'a>
 where T: PartialOrd + Debug + 'a {
     Box::new(move |actual: &T| matchresult_from_comparison!(actual <= expected, "less_than_or_equal"))
 }
 /// Matches if the asserted value is less than or equal to the expected value.
+///
+/// This is the same as [less_than_or_equal].
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), leq(3));
+/// assert_that!(&(1+1), leq(2));
+/// # }
 pub fn leq<'a, T: PartialOrd + Debug + 'a>(expected: T) -> Box<Matcher<'a,T> + 'a> { less_than_or_equal(expected) }
 
 /// Matches if the asserted value is greater than or equal to the expected value.
+///
+/// This is the same as [geq].
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), greater_than_or_equal(1));
+/// assert_that!(&(1+1), greater_than_or_equal(2));
+/// # }
 pub fn greater_than_or_equal<'a, T>(expected: T) -> Box<Matcher<'a,T> + 'a>
 where T: PartialOrd + Debug + 'a {
     Box::new(move |actual: &T| matchresult_from_comparison!(actual >= expected, "greater_than_or_equal"))
 }
 /// Matches if the asserted value is greater than or equal to the expected value.
+///
+/// This is the same as [greater_than_or_equal].
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1+1), geq(1));
+/// assert_that!(&(1+1), geq(2));
+/// # }
 pub fn geq<'a, T: PartialOrd + Debug + 'a>(expected: T) -> Box<Matcher<'a,T> + 'a> { greater_than_or_equal(expected) }
 
 /// Matches if the asserted value is in an epsilon range around the expected value.
 ///
 /// If floating point values are compared for equality this matcher should be used instead of [equal_to]
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// assert_that!(&(1.2 + 3.14), close_to(4.34, 0.00001));
+/// # }
 pub fn close_to<'a, T>(expected: T, eps: T) -> Box<Matcher<'a,T> + 'a>
 where T: Copy + PartialOrd + std::ops::Add<Output=T> + std::ops::Sub<Output=T> + Debug + 'a {
     Box::new(move |actual: &T| {
@@ -137,6 +313,24 @@ where T: Copy + PartialOrd + std::ops::Add<Output=T> + std::ops::Sub<Output=T> +
 /// Matches if asserted value and the expected value are truely the same object.
 ///
 /// The two values are the same if the reside at the same memory address.
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// #[derive(Debug)]
+/// struct Foo;
+/// let foo1 = Foo;
+/// let foo2 = Foo;
+///
+/// assert_that!(&foo1, same_object(&foo1));
+///
+/// assert_that!(
+///     assert_that!(&foo1, same_object(&foo2)),
+///     panics
+/// );
+/// # }
 pub fn same_object<'a, T>(expected: &'a T) -> Box<Matcher<'a,T> + 'a>
 where T: Debug + 'a {
     Box::new(move |actual: &T| {
@@ -149,11 +343,60 @@ where T: Debug + 'a {
     })
 }
 
+/// Write patterns of structs/enums which use `Matcher`s instead of field values.
+///
+/// When providing matchers for multiple fields, not that *all* matchers will be evaluated.
+/// Even if one of them returns `MatchResult::Failed`.
+///
+/// For struct-like structs/enum-variants not all fields need to be listed in the pattern.
+/// Unwanted fields can safely be ommitted. For tuple-like structs/enum-variants all fields
+/// need to be listed in the correct order. Although you can use `any_value()`
+/// to effectively ignore the field.
+///
+/// Note that the correct brace/bracket style for tuple-like structs/enums is `Variant[any_value(), any_value()]`
+/// not `Variant(any_value(), any_value())`. This discrepancy is due to macro parsing reasons.
+///
+/// #Examples
+/// ```rust
+/// // Matching a struct-like ...
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// # fn main() {
+/// struct Foo { x: i32, y: f64 }
+/// let foo = Foo { x: 12, y: 23.4 };
+/// assert_that!(&foo, has_structure!(Foo {
+///     x: eq(12),
+///     y: lt(25.0)
+/// }));
+/// assert_that!(&foo, has_structure!(Foo {
+///     y: lt(25.0) // not all fields need to be given for struct-likes
+/// }));
+///
+/// // Matching a tuple-like ...
+/// struct Bar(i32, f64);
+/// let bar = Bar(12, 23.4);
+/// assert_that!(&bar, has_structure!(Bar[ eq(12), lt(25.0) ]));
+///
+/// // Matching enum variants ...
+/// enum Baz {
+///     Var1 { x: i32, y: f64 },
+///     Var2(i32, f64)
+/// }
+/// let var1 = Baz::Var1 { x: 12, y: 23.4 };
+/// assert_that!(&var1, has_structure!(Baz::Var1 {
+///     x: eq(12),
+///     y: lt(25.0)
+/// }));
+///
+/// let var2 = Baz::Var2(12, 23.4);
+/// assert_that!(&var2, has_structure!(Baz::Var2 [eq(12), lt(25.0)] ));
+/// # }
 #[macro_export]
 macro_rules! has_structure {
     ( $variant:path { $( $field:ident : $matcher:expr ),* $(,)* } ) => { structure!($variant { $($field : $matcher),* }) };
-    ( $variant:path [ $( $matchers:expr ),* ] ) => { structure!($variants [ $($matchers),* ]) }
+    ( $variant:path [ $( $matchers:expr ),* ] ) => { structure!($variant [ $($matchers),* ]) }
 }
+/// Shorter name for `has_structure!`.
 #[macro_export]
 macro_rules! structure {
     ( $variant:path { $( $field:ident : $matcher:expr ),* $(,)* } ) => {
