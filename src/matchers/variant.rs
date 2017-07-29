958f9153adaa28a10406ba43bf50829aeef357e5
Christopher Bacher <mindsbackyard@gmail.com>
2017-07-29 18:20:00 +0200

    add examples to docs for variant matchers

--- src/matchers/variant.rs
+++ src/matchers/variant.rs
@@ -21,14 +21,22 @@ use super::super::*;
 ///
 /// # Examples
 /// If the enum's variants are already imported one can write:
-/// ```rust,ignore
-/// assert_that!(Ok(4), is_variant!(Ok));
-/// ```rust,ignore
-/// If not then the full path of the variant has to be used:
-/// ```rust,ignore
-/// enum MyEnum { Foo, Bar(i32), Baz{x: i32} }
 ///
-/// assert_that!(MyEnum::Baz{x: 2}, is_variant!(MyEnum::Baz));
+/// ```
+/// # #[macro_use] extern crate galvanic_assert;
+/// # fn main() {
+/// let ok: Result<i32, ()> = Ok(4);
+/// assert_that!(&ok, is_variant!(Ok));
+/// # }
+/// ```
+/// If not then the full path of the variant has to be used:
+///
+/// ```
+/// # #[macro_use] extern crate galvanic_assert;
+/// # fn main() {
+/// enum MyEnum { Foo, Bar(i32), Baz{x: i32} }
+/// assert_that!(&MyEnum::Baz{x: 2}, is_variant!(MyEnum::Baz));
+/// # }
 /// ```
 #[macro_export]
 macro_rules! is_variant {
@@ -47,6 +55,15 @@ macro_rules! is_variant {
 }
 
 /// Matches the contents of an `Option` againts a passed `Matcher`.
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// use galvanic_assert::matchers::variant::*;
+/// # fn main() {
+/// assert_that!(&Some(32), maybe_some(eq(32)));
+/// # }
 pub fn maybe_some<'a, T: 'a>(matcher: Box<Matcher<'a,T> + 'a>) -> Box<Matcher<'a,Option<T>> + 'a> {
     Box::new(move |maybe_actual: &'a Option<T>| {
         maybe_actual.as_ref()
@@ -58,6 +75,16 @@ pub fn maybe_some<'a, T: 'a>(matcher: Box<Matcher<'a,T> + 'a>) -> Box<Matcher<'a
 }
 
 /// Matches the contents of a `Result` if it is `Ok` againts a passed `Matcher`.
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// use galvanic_assert::matchers::variant::*;
+/// # fn main() {
+/// let ok: Result<i32,()> = Ok(32);
+/// assert_that!(&ok, maybe_ok(eq(32)));
+/// # }
 pub fn maybe_ok<'a, T: 'a, E: 'a>(matcher: Box<Matcher<'a,T> + 'a>) -> Box<Matcher<'a,Result<T,E>> + 'a> {
     Box::new(move |maybe_actual: &'a Result<T,E>| {
         match maybe_actual.as_ref() {
@@ -69,6 +96,16 @@ pub fn maybe_ok<'a, T: 'a, E: 'a>(matcher: Box<Matcher<'a,T> + 'a>) -> Box<Match
 }
 
 /// Matches the contents of a `Result` if it is `Err` againts a passed `Matcher`.
+///
+/// #Examples
+/// ```rust
+/// # #[macro_use] extern crate galvanic_assert;
+/// use galvanic_assert::matchers::*;
+/// use galvanic_assert::matchers::variant::*;
+/// # fn main() {
+/// let err: Result<i32,i32> = Err(32);
+/// assert_that!(&err, maybe_err(eq(32)));
+/// # }
 pub fn maybe_err<'a, T: 'a, E: 'a>(matcher: Box<Matcher<'a,E> + 'a>) -> Box<Matcher<'a,Result<T,E>> + 'a> {
     Box::new(move |maybe_actual: &'a Result<T,E>| {
         match maybe_actual.as_ref() {
